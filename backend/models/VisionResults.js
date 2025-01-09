001df323ddc87c8dd1cbf9778ecb7cee1ad3ae12
AroojiNO <noaharooji@gmail.com>
2025-01-08 19:52:12 -0500

    delete

--- backend/models/VisionResults.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const VisionResultSchema = new mongoose.Schema({
-  imageUrl: { type: String, required: true },
-  annotations: { type: Object, required: true },
-  createdAt: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model('VisionResult', VisionResultSchema);
